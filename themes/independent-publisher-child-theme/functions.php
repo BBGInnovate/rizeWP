0bb989a7061b387164890f32c50580c630b28f2e
Brian Williamson <brianwilliamson@brians-mbp.home>
2015-09-23 11:41:41 -0400

    Remove category from meta below title (because it repeats from above)

--- themes/independent-publisher-child-theme/functions.php
+++ themes/independent-publisher-child-theme/functions.php
@@ -151,12 +151,12 @@ function my_custom_sizes( $sizes ) {
 														independent_publisher_posted_author();
 													?>
 												</span>
-												<?php if ( independent_publisher_categorized_blog() ) {
+												<?php /* if ( independent_publisher_categorized_blog() ) {
 													echo independent_publisher_entry_meta_category_prefix() . ' ' . independent_publisher_post_categories( '', true );
-												} ?>
+												} */?>
 												<span class="entry-title-meta-post-date">
 													<span class="sep"> <?php echo apply_filters( 'independent_publisher_entry_meta_separator', '|' ); ?> </span>
-													<?php independent_publisher_posted_on_date() ?>
+													<?php independent_publisher_posted_on_date() ?> 
 												</span>
 												<?php do_action( 'independent_publisher_entry_title_meta', $separator = ' | ' ); ?>
 											</h3>
