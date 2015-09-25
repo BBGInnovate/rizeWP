9532511394d144d3f632200dcc2f58b56db4c9e8
Brian Williamson <brianwilliamson@Brians-MacBook-Pro.local>
2015-09-25 11:54:40 -0400

    fixing links.

--- themes/independent-publisher/footer.php
+++ themes/independent-publisher/footer.php
@@ -26,10 +26,10 @@
 	<div class='maxWidth1200'>
 		<ul>
 			<li><a href='/'>home</a></li>
-			<li><a href='index.php/matters/'>in focus</a></li>
+			<li><a href='/index.php/matters/'>in focus</a></li>
 			<li><a href='https://www.facebook.com/africarizing'>facebook</a></li>
 			<li><a href='https://twitter.com/africarizing'>twitter</a></li>
-			<li><a href='index.php/about/'>mission</a></li>
+			<li><a href='/index.php/about/'>mission</a></li>
 		</ul>
 	</div>
 </div>
