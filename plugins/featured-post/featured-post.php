b6a2acb8850f6fe4bfc25cb39f34aa5db00a6fe7
Joe Flowers <jflowers45@gmail.com>
2015-09-29 17:17:11 -0400

    remove widget functionality from featured post, suppress $_GET warnings

--- plugins/featured-post/featured-post.php
+++ plugins/featured-post/featured-post.php
@@ -27,6 +27,14 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 **************************************************************************/
+
+/*************************************************************************
+CUSTOMIZATIONS BY ODDI:
+Removed Code to enable widget
+Added @ before accessing $_GET vars to suppress warnings 
+**************************************************************************/
+
+
 class Featured_Post
 {
     var $db = NULL;
@@ -82,7 +90,7 @@ class Featured_Post
     function add_views_link($views) {
         $post_type = ((isset($_GET['post_type']) && $_GET['post_type'] != "") ? $_GET['post_type'] : 'post');
         $count = $this->total_featured($post_type);
-        $class = $_GET['post_status'] == 'featured' ? "current" : '';
+        $class = @$_GET['post_status'] == 'featured' ? "current" : '';
         $views['featured'] = "<a class=\"" . $class . "\" id=\"featured-post-filter\" href=\"edit.php?&post_status=featured&post_type={$post_type}\">Featured <span class=\"count\">({$count})</span></a>";
         return $views;
     }
@@ -124,7 +132,7 @@ class Featured_Post
         global $current_user;
         get_currentuserinfo();
         if (current_user_can('edit_posts', $user_id)) {
-            $columns['featured'] = __('Featured');
+            $columns['featured'] = __('In Focus');
         }
         return $columns;
     }
@@ -187,7 +195,7 @@ class Featured_Post
     }
     function admin_pre_get_posts($query) {
         global $wp_query;
-        if (is_admin() && $_GET['post_status'] == 'featured') {
+        if (is_admin() && @$_GET['post_status'] == 'featured') {
             $query->set('meta_key', '_is_featured');
             $query->set('meta_value', 'yes');
         }
@@ -207,89 +215,6 @@ class Featured_Post
         return $query;
     }
 }
-class Featured_Post_Widget extends WP_Widget
-{
-    private $post_types = array();
-    function __construct() {
-        parent::WP_Widget(false, $name = 'Featured Post');
-    }
-    
-    function form($instance) {
-        $title = esc_attr($instance['title']);
-        $type = esc_attr($instance['post_type']);
-        $num = (int)esc_attr($instance['num']);
-        $this->post_types = get_post_types(array(
-            '_builtin' => false,
-        ) , 'names', 'or');
-        $this->post_types['post'] = 'post';
-        $this->post_types['page'] = 'page';
-        ksort($this->post_types);
-        echo "<p>";
-        echo "<label for=\"" . $this->get_field_id('title') . "\">";
-        echo _e('Title:');
-        echo "</label>";
-        echo "<input class=\"widefat\" id=\"" . $this->get_field_id('title') . "\" name=\"" . $this->get_field_name('title') . "\" type=\"text\" value=\"" . $title . "\" />";
-        echo "</p>";
-        echo "<p>";
-        echo "<label for=\"" . $this->get_field_id('post_type') . "\">";
-        echo _e('Post Type:');
-        echo "</label>";
-        echo "<select name = \"" . $this->get_field_name('post_type') . "\" id=\"" . $this->get_field_id('title') . "\" >";
-        foreach ($this->post_types as $key => $post_type) {
-            echo '<option value="' . $key . '"' . ($key == $type ? " selected" : "") . '>' . $key . "</option>";
-        }
-        
-        echo "</select>";
-        echo "</p>";
-        echo "<p>";
-        echo "<label for=\"" . $this->get_field_id('num') . "\">";
-        echo _e('Number To show:');
-        
-        echo "</label>";
-        echo "<input id = \"" . $this->get_field_id('num') . "\" class = \"widefat\" name = \"" . $this->get_field_name('num') . "\" type=\"text\" value =\"" . $num . "\" / >";
-        echo "</p>";
-    }
-    function update($new_instance, $old_instance) {
-        $instance = $old_instance;
-        $instance['title'] = strip_tags($new_instance['title']);
-        $instance['num'] = (int)strip_tags($new_instance['num']);
-        $instance['post_type'] = strip_tags($new_instance['post_type']);
-        if ($instance['num'] < 1) {
-            $instance['num'] = 10;
-        }
-        return $instance;
-    }
-    function widget($args, $instance) {
-        extract($args);
-        $title = apply_filters('widget_title', $instance['title']);
-        echo $before_widget;
-        if ($title) {
-            echo $before_title . $title . $after_title;
-        }
-        echo "<ul class=\"widget-list featured-post-widget featured-post\">";
-        wp_reset_query();
-        global $wp_query;
-        $old_query = $wp_query;
-        $FeaturedPost_query = new WP_Query(array(
-            'post_type' => $instance['post_type'],
-            'showposts' => $instance['num'],
-            'featured' => 'yes',
-            'paged' => 1
-        ));
-        while ($FeaturedPost_query->have_posts()) {
-            $FeaturedPost_query->the_post();
-            echo "<li><a href=\"" . get_permalink() . "\">";
-            echo get_the_title();
-            echo "</a>";
-            echo "</li>";
-        }
-        wp_reset_query();
-        $wp_query = $old_query;
-        echo "</ul>";
-        echo $after_widget;
-        // outputs the content of the widget
-    }
-}
+
 $Featured_Post = new Featured_Post();
 
-add_action('widgets_init', create_function('', 'return register_widget("Featured_Post_Widget");') , 100);
