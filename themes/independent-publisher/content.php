4d68644366cd85fb6a27039c9e5156424d8df01a
Joe Flowers <jflowers45@gmail.com>
2015-09-30 00:54:56 -0400

    more in focus tweets

--- themes/independent-publisher/content.php
+++ themes/independent-publisher/content.php
@@ -151,43 +151,45 @@
 		<?php independent_publisher_continue_reading_link(); ?>
 	<?php endif; */ ?>
 
-	<footer class="entry-meta">
+	<?php if (! $eliminateExcerptAndFullText):  ?>
+		<footer class="entry-meta">
 
-		<?php 
-		/* Show author name and post categories only when post type == post AND 
-		 * we're not showing the first post full content 
-		 */ 
-		?>
-		<?php if ( 'post' == get_post_type() && independent_publisher_is_not_first_post_full_content() ) : // post type == post conditional hides category text for Pages on Search ?>
-			<?php /*independent_publisher_posted_author_cats()*/ ?>
-			<?php independent_publisher_posted_author() ?>
-		<?php endif; ?>
-		<span class="sep sep-byline"> | </span>
-		<?php /* Show post date when show post date option enabled */
-		?>
-		<?php if ( independent_publisher_show_date_entry_meta() ) : ?>
-			<?php echo independent_publisher_get_post_date() ?>
-		<?php endif; ?>
+			<?php 
+			/* Show author name and post categories only when post type == post AND 
+			 * we're not showing the first post full content 
+			 */ 
+			?>
+			<?php if ( 'post' == get_post_type() && independent_publisher_is_not_first_post_full_content() ) : // post type == post conditional hides category text for Pages on Search ?>
+				<?php /*independent_publisher_posted_author_cats()*/ ?>
+				<?php independent_publisher_posted_author() ?>
+			<?php endif; ?>
+			<span class="sep sep-byline"> | </span>
+			<?php /* Show post date when show post date option enabled */
+			?>
+			<?php if ( independent_publisher_show_date_entry_meta() ) : ?>
+				<?php echo independent_publisher_get_post_date() ?>
+			<?php endif; ?>
 
-		<?php 
-		/* Show post word count when post is not password-protected AND 
-		 * this is a Standard post format AND
-		 * post word count option enabled AND 
-		 * we're not showing the first post full content
-		 */
-		?>
-		<?php if ( ! post_password_required() && false === get_post_format() && independent_publisher_show_post_word_count() && independent_publisher_is_not_first_post_full_content() ) : ?>
-			<?php echo independent_publisher_get_post_word_count() ?>
-		<?php endif; ?>
+			<?php 
+			/* Show post word count when post is not password-protected AND 
+			 * this is a Standard post format AND
+			 * post word count option enabled AND 
+			 * we're not showing the first post full content
+			 */
+			?>
+			<?php if ( ! post_password_required() && false === get_post_format() && independent_publisher_show_post_word_count() && independent_publisher_is_not_first_post_full_content() ) : ?>
+				<?php echo independent_publisher_get_post_word_count() ?>
+			<?php endif; ?>
 
-		<?php /* Show comments link only when post is not password-protected AND comments are enabled on this post */ ?>
-		<?php if ( ! post_password_required() && comments_open() && ! independent_publisher_hide_comments() ) : ?>
-			<span class="comments-link"><?php comments_popup_link( __( 'Comment', 'independent-publisher' ), __( '1 Comment', 'independent-publisher' ), __( '% Comments', 'independent-publisher' ) ); ?></span>
-		<?php endif; ?>
+			<?php /* Show comments link only when post is not password-protected AND comments are enabled on this post */ ?>
+			<?php if ( ! post_password_required() && comments_open() && ! independent_publisher_hide_comments() ) : ?>
+				<span class="comments-link"><?php comments_popup_link( __( 'Comment', 'independent-publisher' ), __( '1 Comment', 'independent-publisher' ), __( '% Comments', 'independent-publisher' ) ); ?></span>
+			<?php endif; ?>
 
-		<?php $separator = apply_filters( 'independent_publisher_entry_meta_separator', '|' ); ?>
-		<?php edit_post_link( __( 'Edit', 'independent-publisher' ), '<span class="sep"> ' . $separator . ' </span> <span class="edit-link">', '</span>' ); ?>
+			<?php $separator = apply_filters( 'independent_publisher_entry_meta_separator', '|' ); ?>
+			<?php edit_post_link( __( 'Edit', 'independent-publisher' ), '<span class="sep"> ' . $separator . ' </span> <span class="edit-link">', '</span>' ); ?>
 
-	</footer>
-	<!-- .entry-meta -->
+		</footer>
+		<!-- .entry-meta -->
+	<?php endif; ?>
 </article><!-- #post-<?php the_ID(); ?> -->
