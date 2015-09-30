01a16ee5f4b0fb834bc9bb46ce065cce23399f0e
Brian Williamson <brianwilliamson@brians-mbp.home>
2015-09-30 10:20:42 -0400

    Moving mission to custom page template.

---
+++ themes/independent-publisher-child-theme/mission.php
@@ -0,0 +1,275 @@
+<?php
+
+/**
+ template name: Mission page
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package Independent Publisher
+ * @since   Independent Publisher 1.0
+ */
+
+?>
+
+<div style='background:url(https://africa2.rizing.org/wp-content/uploads/2015/08/background.jpg) center center/cover; width: 100%; height: 400px;'></div>
+
+<?php
+$pageBodyID = "pageMission";
+get_header(); ?>
+
+
+	<div id="primary" class="content-area">
+		<main id="content" class="site-content pagePostByCategory" role="main">
+
+<p>At Africa Rizing, our mission is to connect the next generation of global influencers from across the Continent, throughout the diaspora and around the world. Together we will discover new voices and amplify new ideas that result in lasting change.</p>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Qn6FRPWzW-0" class='youtubeVideo' frameborder="0" allowfullscreen></iframe>
+
+<h3>Connect with us</h3>
+Africa Rizing brings you new forms of digital storytelling across social media networks. By connecting you to our team of digital journalists, designers and developers, our goal is to include the audience in our collaborative, iterative approach to telling the stories that matter to you.
+
+<div id='map-rize' class='map'> </div>
+
+<h3>The team</h3>
+Being active in the communities we cover is at the core of Africa Rizing and our journalists reflect our audience. From Dakar, to Abidjan, Accra and Lagos, our team is dedicated to discovering and amplifying the individuals representing the new Africa.
+
+<div id='staff' class='section'>
+<div class='member'>
+	<div class='photo' style='background:#FFF url(../../wp-content/uploads/2015/08/mugshot_adamjmartin.jpg) center center; background-size: cover;'>
+		<img src='../../wp-content/uploads/2015/08/transparentSquare.png' class='square'/>
+	</div>
+	<div class='profile'>
+		<h4>Adam Martin</h4>
+		<p class='role'>Executive Producer, <a href='http://twitter.com/AdamJMartin'>@AdamJMartin</a></p>
+		<p class='biography'>EP @AfricaRizing #DC #Accra #media #civicTech #diversity tiny letter writer and spotify playlist maker</p>
+	</div>
+</div>
+
+<div class='member'>
+	<div class='photo' style='background:#FFF url(../../wp-content/uploads/2015/08/mugshot_victoria_okoye.jpg) center center; background-size: cover;'>
+		<img src='../../wp-content/uploads/2015/08/transparentSquare.png' class='square'/>
+	</div>
+	<div class='profile'>
+		<h4>Victoria Okoye</h4>
+		<p class='role'>Journalist, <a href='http://twitter.com/Victoria_Okoye'>@Victoria_Okoye</a></p>
+		<p class='biography'>Dreamer, writer, urban planner, @WIEGOGlobal urban advocate. Tweeting #urban development, #design, #publicspaces, #streetculture, etc. Carl Jung fan.</p>
+	</div>
+</div>
+
+<div class='member'>
+	<div class='photo' style='background:#FFF url(../../wp-content/uploads/2015/08/mugshot_melb4freepress.jpg) center center; background-size: cover;'>
+		<img src='../../wp-content/uploads/2015/08/transparentSquare.png' class='square'/>
+	</div>
+	<div class='profile'>
+		<h4>Mel Bailey</h4>
+		<p class='role'>Journalist, <a href='http://twitter.com/MelB4freepress'>@MelB4freepress</a></p>
+		<p class='biography'>Digital Media Specialist @VOA_News in #Dakar Formerly @NBCNews, @NYU Alumna mes tweets n&#39;engage que moi</p>
+	</div>
+</div>
+
+<div class='member'>
+	<div class='photo' style='background:#FFF url(../../wp-content/uploads/2015/08/mugshot_coraliesl.jpg) center center; background-size: cover;'>
+		<img src='../../wp-content/uploads/2015/08/transparentSquare.png' class='square'/>
+	</div>
+	<div class='profile'>
+		<h4>Coralie Saint-Louis</h4>
+		<p class='role'>Social media editor, <a href='http://twitter.com/CoralieSL'>@CoralieSL</a></p>
+		<p class='biography'>I don&#39;t know everything and I&#39;m ok with that. Live &amp; Learn. #ShamelesslyHaitian #HowardUniversityAlumna</p>
+	</div>
+</div>
+
+<div class='member'>
+	<div class='photo' style='background:#FFF url(../../wp-content/uploads/2015/09/mugshot_eonwona.jpg) center center; background-size: cover;'>
+		<img src='../../wp-content/uploads/2015/08/transparentSquare.png' class='square'/>
+	</div>
+	<div class='profile'>
+		<h4>Eunice Onwona</h4>
+		<p class='role'>Journalist, <a href='http://twitter.com/eonwona'>@eonwona</a></p>
+		<p class='biography'>Never enough news, plane rides or food for this girl. Digital Media Specialist #DC #ACC</p>
+	</div>
+</div>
+
+<div class='member'>
+	<div class='photo' style='background:#FFF url(../../wp-content/uploads/2015/08/mugshot_BrianWmson.jpg) center center; background-size: cover;'>
+		<img src='../../wp-content/uploads/2015/08/transparentSquare.png' class='square'/>
+	</div>
+	<div class='profile'>
+		<h4>Brian Williamson</h4>
+		<p class='role'>Designer, <a href='http://twitter.com/BrianWmson'>@BrianWmson</a></p>
+		<p class='biography'>Visual journalist and professional drawer. I design, illustrate, animate and write code to tell stories.</p>
+	</div>
+</div>
+</div>
+<div class='clearAll'></div>
+<div id="creativeCommons"><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a><br />This work by <a xmlns:cc="http://creativecommons.org/ns#" href="http://africa.rizing.org" property="cc:attributionName" rel="cc:attributionURL">Africa Rizing</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.</div>
+
+<style>	#map-rize{width:100%; height: 300px; margin-top: 25px;} h1.entry-title{display:none !important;}</style>
+
+
+<script src='//api.tiles.mapbox.com/mapbox.js/v2.2.0/mapbox.js'></script>
+<link href='//api.tiles.mapbox.com/mapbox.js/v2.2.0/mapbox.css' rel='stylesheet' />
+<script>
+jQuery(document).ready( function() {
+	//Create maps
+	L.mapbox.accessToken = 'pk.eyJ1IjoidmlzdWFsam91cm5hbGlzdCIsImEiOiIwODQxY2VlNDRjNTBkNWY1Mjg2OTk3NWIzMmJjMGJhMSJ9.ZjwAspfFYSc4bijF6XS7hw';
+
+	var geojson = [
+	{
+		"type": "FeatureCollection",
+		"features": [
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-77.016556,
+					38.887226
+				]
+			},
+			"properties": {
+				"title": "Africa Rizing HQ",
+				"description": "From our global headquarters our team of editors and producers are connecting individuals throughout the diaspora to bring new voices into our networks.",
+				"marker-color": "#F7941E",
+				"marker-size": "large",
+				"marker-symbol": "building"
+			}
+		},
+
+		{
+			"type": "Feature",
+			"geometry": {
+			"type": "Point",
+			"coordinates": [
+			  -0.200000,
+			  5.550000
+			]
+			},
+			"properties": {
+			"title": "Victoria Okoye (<a href='http://twitter.com/'>@victoria_okoye</a>) — Accra, Ghana",
+			"description": "<img src='http://184.73.203.85:8080/images/mugshot_Victoria_Okoye.jpg' style='width: 30%; float: left; margin-right: 10px;'/>Dreamer, writer, urban planner, @WIEGOGlobal urban advocate. Tweeting #urban development, #design, #publicspaces, #streetculture, etc. Carl Jung fan.",
+			"marker-color": "#FBB040",
+			"marker-size": "large"
+			}
+		},
+
+		{
+			"type": "Feature",
+			"geometry": {
+			"type": "Point",
+			"coordinates": [
+			  -17.366029,
+			  14.764504
+			]
+			},
+			"properties": {
+			"title": "Mel Bailey (<a href='twitter.com/'>@MelB4freePress</a>) — Dakar, Senegal",
+			"description": "<img src='http://54.243.239.169/brian/africa.rizing/images/mugshot_melb4freepress.jpg' style='width: 30%; float: left; margin-right: 10px;'> Digital Media Specialist @VOA_News in #Dakar Formerly @NBCNews, @NYU Alumna mes tweets n'engage que moi ",
+			"marker-color": "#FBB040",
+			"marker-size": "large"
+			}
+		},
+
+		{
+			"type": "Feature",
+			"geometry": {
+			"type": "Point",
+			"coordinates": [
+			  3.379206,
+			  6.524379
+			]
+			},
+			"properties": {
+			"title": "Lagos, Nigeria",
+			"marker-color": "#FBB041",
+			"marker-size": "large"
+			}
+		}
+	  ]
+	}
+
+	];
+
+	//Create the map.
+	var map = L.mapbox.map('map-rize', 'visualjournalist.mnbadlih', {
+		scrollWheelZoom: false
+	});
+
+	//Add the pins to the map.
+	var myLayer = L.mapbox.featureLayer().addTo(map);
+	myLayer.setGeoJSON(geojson);
+
+
+
+
+	//Check the width of the browser.
+	//If the browser is wider than X pixels, recenter the map to include Washington DC.
+function centerMap(){
+		var w = window.innerWidth;
+		if (w>550){
+			//Fit the map to the markers.
+			map.fitBounds(myLayer.getBounds());
+		}else if (w>450){
+			//Center and zoom the map to west Africa.
+			map.setView([8, -5], 4);
+		}else{
+			map.setView([8, -5], 3);
+		}
+	}
+	centerMap();
+
+
+
+	//Resize YouTube videos proportionately 
+	function resizeIframes(){
+		/*
+		var aspectRatio = jQuery("iframe.youtubeVideo").width()/jQuery("iframe.youtubeVideo").height();
+		var columnWidth = jQuery("#content").width();
+
+		console.log('width '+columnWidth)
+
+		jQuery("iframe.youtubeVideo").width(columnWidth);
+		jQuery("iframe.youtubeVideo").height(columnWidth/aspectRatio);
+		*/
+	}
+
+
+	//Resize stuff proportionately
+	function resizeStuffOnResize(){
+	  waitForFinalEvent(function(){
+			resizeIframes();
+			centerMap();
+	  }, 500, "some unique string");
+	}
+
+	//Wait for the window resize to 'end' before executing a function---------------
+	var waitForFinalEvent = (function () {
+		var timers = {};
+		return function (callback, ms, uniqueId) {
+			if (!uniqueId) {
+				uniqueId = "Don't call this twice without a uniqueId";
+			}
+			if (timers[uniqueId]) {
+				clearTimeout (timers[uniqueId]);
+			}
+			timers[uniqueId] = setTimeout(callback, ms);
+		};
+	})();
+
+	window.addEventListener('resize', function(event){
+		resizeStuffOnResize();
+	});
+
+	resizeStuffOnResize();
+});
+</script>
+
+		</main>
+		<!-- #content .site-content -->
+	</div><!-- #primary .content-area -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
