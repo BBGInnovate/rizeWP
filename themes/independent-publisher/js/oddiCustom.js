44df6c3e24b359bf9bf219f8ed4ad522fec2e170
Brian Williamson <brianwilliamson@Brians-MacBook-Pro.local>
2015-10-05 08:37:29 -0400

    Testing solutions for making menu work on desktop >1200

--- themes/independent-publisher/js/oddiCustom.js
+++ themes/independent-publisher/js/oddiCustom.js
@@ -1,62 +1,60 @@
 jQuery(document).ready(function() { 
-    // 
-    // create social networking pop-ups
-    // link selector and pop-up window size
+	// 
+	// create social networking pop-ups
+	// link selector and pop-up window size
 
-    var shareConfig = {
-        Link: "a.share",
-        Width: 500,
-        Height: 500
-    };
+	var shareConfig = {
+		Link: "a.share",
+		Width: 500,
+		Height: 500
+	};
 
-    // add handler links
-    var shareLink = document.querySelectorAll(shareConfig.Link);
-    for (var a = 0; a < shareLink.length; a++) {
-        shareLink[a].onclick = PopupHandler;
-    }
+	// add handler links
+	var shareLink = document.querySelectorAll(shareConfig.Link);
+	for (var a = 0; a < shareLink.length; a++) {
+		shareLink[a].onclick = PopupHandler;
+	}
 
-    // create popup
-    function PopupHandler(e) {
+	// create popup
+	function PopupHandler(e) {
 
-    	/*you could tweet the highlighted/selected text by encoding and concatenating it with the URL
-    	var text = "";
-        if (window.getSelection) {
-            text = window.getSelection().toString();
-        } else if (document.selection && document.selection.type != "Control") {
-            text = document.selection.createRange().text;
-        }
-        console.log(text);
-    	*/
+		/*you could tweet the highlighted/selected text by encoding and concatenating it with the URL
+		var text = "";
+		if (window.getSelection) {
+			text = window.getSelection().toString();
+		} else if (document.selection && document.selection.type != "Control") {
+			text = document.selection.createRange().text;
+		}
+		console.log(text);
+		*/
 
-        e = (e ? e : window.event);
+		e = (e ? e : window.event);
 
-        //changed e.target.parentNode to e.target when i removed the <img/> tag
-        //var t = (e.target.parentNode ? e.target.parentNode : e.srcElement);
-        var t = (e.target ? e.target : e.srcElement);
-        //logger(t)
+		//changed e.target.parentNode to e.target when i removed the <img/> tag
+		//var t = (e.target.parentNode ? e.target.parentNode : e.srcElement);
+		var t = (e.target ? e.target : e.srcElement);
+		//logger(t)
 
-        // popup position
-        var px = Math.floor(((screen.availWidth || 1024) - shareConfig.Width) / 2),
-            py = Math.floor(((screen.availHeight || 700) - shareConfig.Height) / 2);
+		// popup position
+		var px = Math.floor(((screen.availWidth || 1024) - shareConfig.Width) / 2),
+			py = Math.floor(((screen.availHeight || 700) - shareConfig.Height) / 2);
 
-        // open popup
-        var popup = window.open(t.href, "social", 
-            "width="+shareConfig.Width+",height="+shareConfig.Height+
-            ",left="+px+",top="+py+
-            ",location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1");
-        if (popup) {
-            popup.focus();
-            if (e.preventDefault) e.preventDefault();
-            e.returnValue = false;
-        }
+		// open popup
+		var popup = window.open(t.href, "social", 
+			"width="+shareConfig.Width+",height="+shareConfig.Height+
+			",left="+px+",top="+py+
+			",location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1");
+		if (popup) {
+			popup.focus();
+			if (e.preventDefault) e.preventDefault();
+			e.returnValue = false;
+		}
 
-        return !!popup;
-    }
+		return !!popup;
+	}
 
-    //Dropdown navigation
-    jQuery("#menuButton" ).click(function() {
-        jQuery( "#categoryMenu #dropdown" ).slideToggle("drop");
-        jQuery('#categoryMenu #dropdown').scrollTo( 0 , 100 );
-    });
-        //$( "#container" ).click(function() {$('#menu #dropdown').hide();});
+	//Dropdown navigation
+	jQuery("#menuButton" ).click(function() {
+		jQuery( "#categoryMenu #dropdown" ).slideToggle("drop");
+	});
 });
