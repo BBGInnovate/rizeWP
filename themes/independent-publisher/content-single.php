e193070361bd4b118f1f872f3fa19493bd0f43ff
Brian Williamson <brianwilliamson@Brians-MacBook-Pro.local>
2015-10-07 09:20:28 -0400

    Merging styles from gigi.css to main. Fixing social menu media query.

--- themes/independent-publisher/content-single.php
+++ themes/independent-publisher/content-single.php
@@ -112,7 +112,7 @@
 				<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
 					<img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" />
 				</a><br />
-				This work by <a xmlns:cc="http://creativecommons.org/ns#" href="http://africa.rizing.org" property="cc:attributionName" rel="cc:attributionURL">Africa Rizing</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
+				This work by <a xmlns:cc="http://creativecommons.org/ns#" href="https://africa.rizing.org" property="cc:attributionName" rel="cc:attributionURL">Africa Rizing</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
 			</div>
 		</div>
 
