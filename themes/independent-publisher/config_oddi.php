555e2f32f45b5f2cf1079577ba2d938ed5fad69e
Joe Flowers <jflowers45@gmail.com>
2015-10-09 12:03:47 -0400

    use domain variable for default img

--- themes/independent-publisher/config_oddi.php
+++ themes/independent-publisher/config_oddi.php
@@ -2,7 +2,7 @@
 
 //this file is called in header.php.  These are used for various og/twitter/social media tags
 define("DEFAULT_DESCRIPTION", "Connecting the next generation of global influencers from across the Continent, and around the world to engage in, 'a smarter conversation'");
-define("DEFAULT_IMAGE","https://africa2.rizing.org/wp-content/uploads/2015/09/cropped-Rize-socialprofiles_500.png");
+define("DEFAULT_IMAGE", site_url() . "/wp-content/uploads/2015/09/cropped-Rize-socialprofiles_500.png");
 define("DEFAULT_TITLE",get_bloginfo('name'));
 define("DEFAULT_AUTHOR","");
 define("DEFAULT_KEYWORDS","");
