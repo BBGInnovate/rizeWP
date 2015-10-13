463ed7f60393d07dcded3c533c0796199f2c3258
Joe Flowers <jflowers45@gmail.com>
2015-10-13 12:33:59 -0400

    use actual description of 'trending' category

--- themes/independent-publisher-child-theme/page-inFocus-category.php
+++ themes/independent-publisher-child-theme/page-inFocus-category.php
@@ -14,14 +14,16 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="content" class="site-content" role="main">
 			<h1 class="page-title">Trending</h1>
-			<p class="intro">Our daily digest of the links, trends, hashtags and stories that matter most</p>
 			<?php
 
 				$cat_id=get_cat_id('Trending');
 				if ($cat_id ==0) {
 					//echo "there is no in focus category ... please create one locally"; 
 				} else {
-
+					$catDesc=strip_tags(category_description($cat_id));
+					if ($catDesc != "") {
+						echo "<p class='intro'>$catDesc</p>";	
+					}
 					$qParams=array( 
 						'post_type' => array('post'),
 						'posts_per_page' => 1,
