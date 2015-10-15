b8e37cf5c227057b9ae88e26c1d6833e58ab4963
Joe Flowers <jflowers45@gmail.com>
2015-10-15 11:47:03 -0400

    fix for medium size images

--- themes/independent-publisher-child-theme/functions.php
+++ themes/independent-publisher-child-theme/functions.php
@@ -448,4 +448,45 @@ function my_custom_sizes( $sizes ) {
 		add_action( 'manage_category_custom_column' , 'xg_featured_category_columns_values', 10, 3 );
 	}
 	
+
+	/* ODDI CUSTOM: we are using this filter: https://codex.wordpress.org/Plugin_API/Filter_Reference/img_caption_shortcode
+	and we took the original function definition from media.php as our starting point ****/
+	add_filter( 'img_caption_shortcode', 'my_img_caption_shortcode', 10, 3 );
+
+	function my_img_caption_shortcode( $empty, $attr, $content ){
+		$atts = shortcode_atts( array(
+			'id'	  => '',
+			'align'	  => 'alignnone',
+			'width'	  => '',
+			'caption' => '',
+			'class'   => '',
+		), $attr, 'caption' );
+
+		$atts['width'] = (int) $atts['width'];
+		if ( $atts['width'] < 1 || empty( $atts['caption'] ) )
+			return $content;
+
+		if ( ! empty( $atts['id'] ) )
+			$atts['id'] = 'id="' . esc_attr( $atts['id'] ) . '" ';
+
+		$class = trim( 'wp-caption ' . $atts['align'] . ' ' . $atts['class'] );
+
+		if ( current_theme_supports( 'html5', 'caption' ) ) {
+			return '<figure ' . $atts['id'] . 'style="width: ' . (int) $atts['width'] . 'px;" class="' . esc_attr( $class ) . '">'
+			. do_shortcode( $content ) . '<figcaption class="wp-caption-text">' . $atts['caption'] . '</figcaption></figure>';
+		}
+
+		$caption_width = 10 + $atts['width'];
+
+		$style = '';
+		/* ODDI CUSTOM: we don't want the width applied 
+		if ( $caption_width )
+			$style = 'style="width: ' . (int) $caption_width . 'px" ';
+		*/	
+
+		return '<div ' . $atts['id'] . $style . 'class="' . esc_attr( $class ) . '">'
+		. do_shortcode( $content ) . '<p class="wp-caption-text">' . $atts['caption'] . '</p></div>';
+
+	}
+
 	
