61de353a60256552681dffafc4064838dfd8aae2
Joe Flowers <jflowers45@gmail.com>
2015-10-21 14:16:50 -0400

    option to display rss from fuego

--- fuego/fuego.php
+++ fuego/fuego.php
@@ -10,7 +10,11 @@
   *   An API key must be set in config.php. Default FALSE.
  */
 
+
+
 require('./init.php');
+
+
 use OpenFuego\app\Getter as Getter;
 
 
@@ -18,13 +22,68 @@ $requireQuality=true;
 if ( isset ( $_GET['showAll']) ) {
    $requireQuality=false;
 } 
+$displayFormat="json";
+if ( isset ( $_GET['rss'] ) ) {
+	$displayFormat='rss';
+}
 
 $fuego = new Getter();
 $items = $fuego->getItems(20, 24, $requireQuality, TRUE); // quantity, hours, scoring, metadata
 
-print "<a href='fuego.php'>BE SMART</a> | <a href='fuego.php?showAll'>MOST RECENT 20</a><BR><BR>";
+if ($displayFormat=='json') {
+	print "<a href='fuego.php'>BE SMART</a> | <a href='fuego.php?showAll'>MOST RECENT 20</a> | <a target='blank' href='fuego.php?showAll&rss'>RSS ALL</a> | <a target='blank' href='fuego.php?rss'>RSS SMART</a><BR><BR>";
+	print '<pre>';
+	print_r($items);
+	print '</pre>';
+} else if ($displayFormat=='rss') {
+	echo '
+<?xml version="1.0" ?>
+<rss version="0.92">
+	<channel>
+		<title>RIZE FUEGO FEED</title>
+		<description>A feed used by RIZE team to go to fuego</description>
+		<link>http://africa.rizing.org</link>
+		<language>en-us</language>
+	';
+	if ( $items ) {
+		foreach ($items as $key => $item) {
+			$title = $item['tw_text'];
+			$link = $item['url'];
+			$desc = $item['tw_text'];
 
-print '<pre>';
-print_r($items);
-print '</pre>';
+			/* often times some metadata values are set and others aren't, so we check each one.  The fuego backend process fills this section using an embed.ly api key */
+			if ( isset ($item['metadata']) ) {
+				$m = $item['metadata'];
+
+				if ( isset ( $m['title'] ) ) {
+					$title = $m['title'];	
+				}
+				if ( isset ($m['url'] ) ) {
+					$url = $m['url'];
+				}
+				
+				if ( isset ($m['description'] ) ) {
+					$desc = $m['description'];
+				}
+			}
+			echo '
+			<item>
+				<title>' . $title . '</title>
+				<link>' . $url . '</link>
+				<description>
+					<![CDATA[
+					' . $desc . '
+					]]>
+				</description>
+			</item>
+			';
+		};
+	}
+	echo '
+	</channel>
+</rss>
+	';
+}
+
+
 ?>
