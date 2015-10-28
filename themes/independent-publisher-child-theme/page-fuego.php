dfcc52270af5d25ce8b960bb259830f75b7aec96
Brian Williamson <brianwilliamson@Brians-MacBook-Pro.local>
2015-10-28 10:29:32 -0400

    Trying to fix medium.com links in tweets (because the url structure includes an optional @ sign)

--- themes/independent-publisher-child-theme/page-fuego.php
+++ themes/independent-publisher-child-theme/page-fuego.php
@@ -26,10 +26,12 @@ get_header(); ?>
 		$counter=0;
 
 		function twitterify($ret) {
+			$ret = str_replace("https://medium.com/@","https://medium.com/", $ret);
+
 			$ret = preg_replace("#(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t< ]*)#", "\\1<a href=\"\\2\" target=\"_blank\">\\2", $ret);
-			$ret = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r< ]*)#", "\\1<a href=\"http://\\2\" target=\"_blank\">\\2", $ret);
-			$ret = preg_replace("/@(\w+)/", "<a href=\"http://www.twitter.com/\\1\" target=\"_blank\">@\\1</a>", $ret);
-			$ret = preg_replace("/#(\w+)/", "<a href=\"http://search.twitter.com/search?q=\\1\" target=\"_blank\">#\\1</a>", $ret);
+			$ret = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r< ]*)#", "\\1<a href=\"https://\\2\" target=\"_blank\">\\2", $ret);
+			$ret = preg_replace("/@(\w+)/", "<a href=\"https://www.twitter.com/\\1\" target=\"_blank\">@\\1</a>", $ret);
+			$ret = preg_replace("/#(\w+)/", "<a href=\"https://search.twitter.com/search?q=\\1\" target=\"_blank\">#\\1</a>", $ret);
 			return $ret;
 		}
 
