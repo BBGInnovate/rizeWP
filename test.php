654d72c26410caae4e4334e7e404e620cdf813e7
Brian Williamson <brianwilliamson@Brians-MacBook-Pro.local>
2015-10-29 16:44:13 -0400

    testing twitter card.

---
+++ test.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title></title>
+		<!-- for Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta name="twitter:site" content="@AfricaRizing">
+	<meta name="twitter:creator" content="@eonwona">	<meta property="twitter:title" content="Kids in Accra are Growing Green with Environment360">
+	<meta property="twitter:description" content="ACCRA, GHANA -- Inspired by hopes of a greener future, more inclusive communities and the smiles of young children, Cordie Aziz and her team at Environment360 are working to improve the lives of children in Accra's coastal and mining communities, where poor environmental and sanitary conditions abound.">
+	<meta property="twitter:image" content="https://africa.rizing.org/wp-content/uploads/2015/10/Jaku-and-the-Earth-Defenders.jpg">
+	<meta property="twitter:url" content="https://africa.rizing.org/kids-in-accra-are-growing-green-with-environment360/">
+
+</head>
+<body>
+<h1>test</h1>
+</body>
+</html>
