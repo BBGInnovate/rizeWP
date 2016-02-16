5e5f5dfa3d378a02d3578093976ec086eace6ad7
Joe Flowers <jflowers45@gmail.com>
2016-02-16 13:38:31 -0500

    let trending hack page be picked up by template

--- themes/independent-publisher-child-theme/page-trending-curl.php
+++ themes/independent-publisher-child-theme/page-trending-curl.php
@@ -1,6 +1,13 @@
-
-
 <?php
+
+/**
+ * The Template for displaying all single posts.
+ *
+ * @package Independent Publisher
+ * @since   Independent Publisher 1.0 
+  template name: Trending Hack
+
+ */
 
 error_reporting(E_ALL);
 $ch = curl_init( "https://africa2.rizing.org/trending/");
