b4c8a9e4cc0e5c2907e6ae62c806470dd2b01053
Joe Flowers <jflowers45@gmail.com>
2016-02-16 13:20:13 -0500

    trending hack

---
+++ themes/independent-publisher-child-theme/page-trending-curl.php
@@ -0,0 +1,14 @@
+
+
+<?php
+
+error_reporting(E_ALL);
+$ch = curl_init( "https://africa2.rizing.org/trending/");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result=curl_exec ($ch);
+curl_close ($ch);
+$result=str_replace("https://africa2.rizing.org","https://africa.rizing.org",$result);
+$result=str_replace("url(/wp-content/","url(https://africa2.rizing.org/wp-content/",$result);
+echo $result;
+ 
+ ?>
